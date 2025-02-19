392e2de525b7f39c4bfbcb668fc5846945cd8f16
rspartan-117 <riteksharmachd@gmail.com>
2025-02-19 22:00:00 +0530

    Fixed Layout

--- mfe1/src/App.js
+++ mfe1/src/App.js
@@ -5,21 +5,20 @@ import RemoteApp2 from "app3/App";
 const App = () => {
 
   return (
-<div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
+<div style={{ display: "flex", flexDirection: "column"}}>
   {/* Header */}
   <div style={{textAlign: "center"}}>
     <h1>Proof of Concept : Host App</h1>
   </div>
   {/* Remote Apps */}
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "center", // Centers horizontally
-      alignItems: "center", // Centers vertically // 
-      padding: "20px",
-      margin:"200px", // Adds padding to prevent elements from touching screen edges// 
-    }}
-  >
+  <div style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start", // Prevents shifting when height changes
+          gap: "20px", // Adds spacing between components
+          padding: "20px",
+        }}
+      >
     <Suspense fallback={"loading..."}>
       <RemoteApp2 />
       <RemoteApp />
